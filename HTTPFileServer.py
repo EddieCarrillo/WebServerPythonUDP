1a941db66a1eded1def82657f2e9286168a66842
edcarri <edcarri@amazon.com>
2020-03-18 05:22:57 -0700

    Adds server logic. Tested and works.

--- HTTPFileServer.py
+++ HTTPFileServer.py
@@ -1,26 +1,53 @@
 #import socket module
 from socket import *
 import sys # In order to terminate the program
-serverSocket = socket(AF_INET, SOCK_STREAM) #Prepare a sever socket
-#Fill in start
-#Fill in end
-while True:
-#Establish the connection print('Ready to serve...') connectionSocket, addr = try:
-#Fill in start
-#Fill in end
-message = #Fill in start #Fill in end filename = message.split()[1]
-f = open(filename[1:])
-outputdata = #Fill in start #Fill in end #Send one HTTP header line into socket
-#Fill in start
-#Fill in end
-#Send the content of the requested file to the client for i in range(0, len(outputdata)):
-connectionSocket.send(outputdata[i].encode()) connectionSocket.send("\r\n".encode())
-connectionSocket.close() except IOError:
-#Send response message for file not found
-    #Fill in start
-#Fill in end
-#Close client socket
-        #Fill in start
-#Fill in end
+
+httpVersion = 'HTTP/1.1'
+
+okPhrase = "OK"
+okStatusCode = 200
+goodStatusLine = f'{httpVersion} {okStatusCode} {okPhrase} \r\n'
+
+missingFilePhrase = "Not Found"
+missingFileStatusCode = 404
+missingFileStatusLine = f'{httpVersion} {missingFileStatusCode} {missingFilePhrase} \r\n'
+
+contentTypeHeaderLine = 'ContentType: text/html \r\n'
+
+serverPort = 12346
+#Set up a TCP socket.
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Associate the socket to this port
+serverSocket.bind(('', serverPort))
+#Start listening for TCP connections.
+serverSocket.listen(1)
+print('The server is listening for connections...')
+
+while True:  
+    #Establish a connection.  
+    print("Ready to serve...")
+    #Create a socket dedicated to this particular client.
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        #Get the URL and remove first forward slash in HTTP message
+        filename = (message.split()[1])[1:]
+        f = open(filename)
+        outputData = f.read()   
+        #Send one HTTP status line into the socket
+        connectionSocket.send(goodStatusLine.encode())
+        connectionSocket.send(contentTypeHeaderLine.encode())
+        connectionSocket.send('\r\n'.encode()) #Data is comming up next.
+
+        connectionSocket.send(outputData.encode())
+        connectionSocket.send('\r\n'.encode())
+
+        connectionSocket.close()
+    except IOError:
+        #Send response message for file not found.
+        connectionSocket.send(missingFileStatusLine.encode())
+        connectionSocket.close()
+
+
 serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+sys.exit()
